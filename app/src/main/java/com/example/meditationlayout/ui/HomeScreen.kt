a27080fba144f047663659ad8ad3da320ba53be0
Vitor Soares Silva <vitaoosp@gmail.com>
2021-07-11 01:13:38 -0300

    greetings and meditation state

---
+++ app/src/main/java/com/example/meditationlayout/ui/HomeScreen.kt
@@ -0,0 +1,114 @@
+package com.example.meditationlayout.ui
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.meditationlayout.R
+import com.example.meditationlayout.ui.theme.*
+
+@Composable
+fun HomeScreen() {
+    Box(
+        modifier = Modifier
+            .background(DeepBlue)
+            .fillMaxSize()
+    ) {
+        Column {
+            GreetingSection()
+            ChipSection(chips = listOf("Bons Sonhos", "Insonia", "Depressão"))
+        }
+
+    }
+}
+
+@Composable
+fun GreetingSection(
+    name: String = "Vitor Soares Silva"
+) {
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(15.dp)
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+
+            ) {
+            Text(
+                text = "Bom Dia, $name",
+                style = MaterialTheme.typography.h2
+            )
+            Text(
+                text = "Desejo a você um otimo dia, $name",
+                style = MaterialTheme.typography.body1
+            )
+        }
+        Icon(
+            painter = painterResource(id = R.drawable.ic_search),
+            contentDescription = "Search",
+            tint = Color.White,
+            modifier = Modifier.size(24.dp)
+        )
+    }
+}
+
+@Composable
+fun ChipSection(
+    chips: List<String>
+) {
+    var selectedChipIndex by remember {
+        mutableStateOf(0)
+    }
+
+    LazyRow {
+        items(chips.size) {
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier
+                    .padding(start = 15.dp, top = 15.dp, bottom = 15.dp)
+                    .clickable {
+                        selectedChipIndex = it
+                    }
+                    .clip(RoundedCornerShape(10.dp))
+                    .background(
+                        if (selectedChipIndex == it) ButtonBlue
+                        else DarkerButtonBlue
+                    )
+                    .padding(15.dp)
+
+            ) {
+                Text(text = chips[it], color = TextWhite)
+            }
+        }
+    }
+}
+
+@Composable
+fun CurrentMeditation(
+    color: Color = LightRed
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+
+    ) {
+
+    }
+}
